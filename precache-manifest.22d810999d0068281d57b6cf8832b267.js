b8b3be8c49939ba59ebf923ea44ef3cb7d2af795
Prasun Bhunia <prasunbhunia99@gmail.com>
2020-07-05 17:57:21 +0530

    Updates

---
+++ precache-manifest.22d810999d0068281d57b6cf8832b267.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2529823e34f5457ac7413a0a5b4342",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "fc433c38104dbfb47e1e",
+    "url": "/monster-rolodex/static/css/main.de81c769.chunk.css"
+  },
+  {
+    "revision": "2126805bdc385eae7cd9",
+    "url": "/monster-rolodex/static/js/2.0f1a2c81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.0f1a2c81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc433c38104dbfb47e1e",
+    "url": "/monster-rolodex/static/js/main.6ef205d8.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
